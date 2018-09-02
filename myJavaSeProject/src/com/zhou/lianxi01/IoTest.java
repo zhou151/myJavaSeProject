7a130ab519492b3057924dc968aacbec67ffd083
zhou151 <1045907858@qq.com>
2018-09-02 14:17:46 +0800

    9/2-upadte

--- myJavaSeProject/src/com/zhou/lianxi01/IoTest.java
+++ myJavaSeProject/src/com/zhou/lianxi01/IoTest.java
@@ -1,10 +1,14 @@
 package com.zhou.lianxi01;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.Reader;
 import java.io.Writer;
 
@@ -13,7 +17,122 @@ public class IoTest
 	public static void main(String[] args)
 	{
 		//fileOut();
-		fileInput();
+		//fileInput();
+		//myOutPutstream();
+		//myInputstream();
+		myCopy();
+	}
+
+	
+	/**
+	 * 
+	 * @fun-name myCopy
+	 * @return-type void
+	 * @author Mr_zhou
+	 * @date 2018年9月2日 下午1:30:04
+	 * TODO 字节流复制文件操作
+	 */
+	private static void myCopy()
+	{
+		
+		try
+		{
+			//-----------------------------------------------------
+			File file = new File("D:/tx.jpg");
+			InputStream fileInputStream = new FileInputStream(file);
+			
+			//这里的file01如果不存在会自动创建
+			File file01 = new File("E:/tx.jpg");
+			OutputStream fileOutputStream = new FileOutputStream(file01);
+			
+			byte [] byteArr=new byte[8];
+			for(int len=0;(len=fileInputStream.read(byteArr))!=-1;)
+			{
+				fileOutputStream.write(byteArr, 0, len);
+				fileOutputStream.flush();
+			}
+			//注意关闭流,先开后关
+			fileOutputStream.close();
+			fileInputStream.close();
+			System.out.println("完成");
+			//-----------------------------------------------------
+		} catch (FileNotFoundException e)
+		{
+			e.printStackTrace();
+		} catch (IOException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * 
+	 * @fun-name myInputstream
+	 * @return-type void
+	 * @author Mr_zhou
+	 * @date 2018年9月2日 下午12:12:17
+	 * TODO 字节输入流,(读文件)
+	 * 接口  InputStream
+	 * 实现类  FileInputStream
+	 */
+	private static void myInputstream()
+	{
+		try
+		{
+			//-----------------------------------------------------
+			File file = new File("D:/a.txt");
+			InputStream fileInputStream = new FileInputStream(file);
+			//读小文件建议写法
+			/*byte [] byteArr=new byte[(int)file.length()];
+			fileInputStream.read(byteArr);*/
+			
+			//方式二,边读边输出,每次读8个字符,性能更高,建议写法
+			byte [] byteArr=new byte[8];
+			int len=0;
+			for(;(len=fileInputStream.read(byteArr))!=-1;)
+			{
+				System.out.println(new String(byteArr,0,len));
+			}
+			fileInputStream.close();
+			//-----------------------------------------------------
+		} catch (FileNotFoundException e)
+		{
+			e.printStackTrace();
+		} catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * 
+	 * @fun-name MyoutPutstream
+	 * @return-type void
+	 * @author Mr_zhou
+	 * @date 2018年9月2日 下午12:07:16
+	 * TODO  字节输出流输出文本demo
+	 * 接口 OutputStream
+	 * 实现类  FileOutputStream
+	 */
+	private static void myOutPutstream()
+	{
+		try
+		{
+			//-----------------------------------------------------
+			OutputStream fileOutputStream = new FileOutputStream(new File(""),true);
+			
+			String str="sddssd";
+			
+			fileOutputStream.write(str.getBytes());
+			//-----------------------------------------------------
+		} catch (FileNotFoundException e)
+		{
+			e.printStackTrace();
+		} catch (IOException e)
+		{
+			e.printStackTrace();
+		}
 	}
 
 	/**
@@ -69,6 +188,7 @@ public class IoTest
 	{
 		try
 		{
+			//-----------------------------------------------------
 			File file = new File("D:/a.txt");
 			//注意参数2为true为追加内容
 			Writer fileWriter = new FileWriter(file,true);
@@ -78,6 +198,7 @@ public class IoTest
 			fileWriter.flush();
 			//一定要关闭
 			fileWriter.close();
+			//-----------------------------------------------------
 		} catch (IOException e)
 		{
 			e.printStackTrace();
