4bf8246f8ed2ad4ac87667b5f22d06cce759d90c
zhou151 <1045907858@qq.com>
2018-11-14 14:02:56 +0800

    --11-14

--- myJavaSeProject/src/cn/zhou/Test/Test1.java
+++ myJavaSeProject/src/cn/zhou/Test/Test1.java
@@ -7,11 +7,30 @@ public class Test1
 	public static void main(String[] args)
 	{
 		 
-		((Test1)o).test(new UsbTestImp());
+		/*
+		 * 基本数据类型
+			byte short int long  整形
+			float double 	浮点
+			boolean 	逻辑
+			char	字符
+			
+		*/
+		
+		
+		String str="str";
+		Test05 test05 = new  Test1().new Test05();
+		
+		
 	}
 	public void  test(UsbTest u)
 	{
 		u.open();
 		System.out.println("ok");
+	}
+	
+	class Test05{
+		void print(){
+			System.out.println("内部类打印");
+		}
 	}
 }
